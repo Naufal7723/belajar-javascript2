90113ed5be735ba6b19d3efc46d286974f67523f
Naufal7723 <naufalff128@gmail.com>
2023-10-10 11:05:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var nilai1 = 70;
+var nilai2 = 80;
+
+if (nilai1 >= 60 && nilai2 >= 60) {
+    console.log("Anda lulus kedua ujian.");
+} else {
+    console.log("Anda tidak lulus salah satu ujian.");
+}
+for (var i = 0; i < 5; i++) {
+    console.log("Iterasi ke-" + i);
+}
+var i = 0;
+while (i < 5) {
+    console.log("Iterasi ke-" + i);
+    i++;
+}
